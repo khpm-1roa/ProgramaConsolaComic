140a5630157b9a826b33002bb28e091b53b6dc38
PILLAJO MONTACHANA KEVIN HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-07-23 19:56:44 -0500

    Add files via upload

---
+++ ProyectoNinject1/ProyectoNinject1/Libreria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoNinject1
+{
+    public class Libreria : ILibrero
+    {
+        public string InsertarLibro()
+        {
+            string Libro = "Se a ingresado el libro exitosamente con ninject";
+            Console.WriteLine(Libro);
+            return Libro;
+        }
+    }
+}
+
---
+++ ProyectoNinject1/ProyectoNinject1/NinjectBinding.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProyectoNinject1
+{
+    public class NinjectBinding:Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILibrero>().To<Libreria>();
+        }
+    }
+}
